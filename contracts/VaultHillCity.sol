bfe490598e7012ab5fe3864317d6711fd3584647
umairJanjua <56170485+UmairJanjua6@users.noreply.github.com>
2021-08-25 21:36:52 +0500

    first commit

---
+++ contracts/VaultHillCity.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: unlicenced
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract VaultHillCity is ERC20 {
+    constructor() ERC20("Vault Hill City", "VHC") {
+        _mint(msg.sender, 340000000 * 10 ** uint256(decimals()));
+    }
+}
---
+++ migrations/2_deploy.js
@@ -0,0 +1,5 @@
+const VaultHillCity = artifacts.require("VaultHillCity");
+
+module.exports = function (deployer) {
+  deployer.deploy(VaultHillCity);
+};
